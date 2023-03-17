f2b597b0b99a5798a1bc858a6fbb13ee762eebe7
Chad Duck <admin@chadduck.net>
2023-03-17 04:43:04 -0500

    fixing stuff up

--- src/App.vue
+++ src/App.vue
@@ -24,14 +24,20 @@ export default {
 
         checkAgent(userAgent) {
             switch (true) {
-                case userAgent.indexOf("edge") > -1: return "MS Edge";
-                case userAgent.indexOf("edg/") > -1: return "Edge ( chromium based)";
-                // case userAgent.indexOf("iphone") > -1: return "iPhone";
+                case userAgent.indexOf("edge") > -1: return "EdgeOG";
+
+                case userAgent.indexOf("edg/") > -1: return "Edge";
+
                 case userAgent.indexOf("opr") > -1 && !!window.opr: return "Opera";
+
                 case userAgent.indexOf("chrome") > -1 && !!window.chrome: return "Chrome";
-                case userAgent.indexOf("trident") > -1: return "MS IE";
-                case userAgent.indexOf("firefox") > -1: return "Mozilla Firefox";
+
+                case userAgent.indexOf("trident") > -1: return "IE";
+
+                case userAgent.indexOf("firefox") > -1: return "Firefox";
+
                 case userAgent.indexOf("safari") > -1: return "Safari";
+                
                 default: return false;
             }
         },
