3f047f9b2badd8bf07e00e9ab3ea03cf4f82c4dc
Chad Duck <admin@chadduck.net>
2023-03-17 04:41:31 -0500

    fixed iOS Safari support

--- src/App.vue
+++ src/App.vue
@@ -26,7 +26,7 @@ export default {
             switch (true) {
                 case userAgent.indexOf("edge") > -1: return "MS Edge";
                 case userAgent.indexOf("edg/") > -1: return "Edge ( chromium based)";
-                case userAgent.indexOf("iphone") > -1: return "iPhone";
+                // case userAgent.indexOf("iphone") > -1: return "iPhone";
                 case userAgent.indexOf("opr") > -1 && !!window.opr: return "Opera";
                 case userAgent.indexOf("chrome") > -1 && !!window.chrome: return "Chrome";
                 case userAgent.indexOf("trident") > -1: return "MS IE";
