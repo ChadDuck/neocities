4bf0e0f65ca315859fe3977f4f3d98323f98d33b
Chad Duck <admin@chadduck.net>
2023-03-17 02:55:26 -0500

    Added GH link, test removal async js audio fetch

---
+++ dev/pw-template.js
@@ -0,0 +1,4 @@
+export const apiKey = { user: "Your username here", pass: "Your password here" };
+
+// Here's the neocities js api github
+// https://github.com/neocities/neocities-node
--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@ export default {
             audioLink: "https://files.catbox.moe/9lryje.ogg",
             audioBlob: false,
             duckClicked: false,
+            duckInfoClicked: false,
         }
     },
 
@@ -41,9 +42,10 @@ export default {
         },
 
         playAudio() {
+            // Javascript client-side loading bypasses hotlinking rule
             if (!this.duckClicked) {
                 this.duckClicked = true;
-                let audio = new Audio(this.audioBlob);
+                let audio = new Audio(this.audioLink);
                 audio.loop = true;
                 audio.play();
             }
@@ -51,13 +53,17 @@ export default {
     },
 
     async mounted() {
-        await this.fetchAudio();
+        // await this.fetchAudio();
     },
 }
 </script>
 
 <template lang="pug">
-img(@click="playAudio()" :class="{ 'duck-rotate': duckClicked }" src="/img/duckie-transparent.gif")
+img.duck(@click="playAudio()" :class="{ 'duck-rotate': duckClicked }" src="/img/duckie-transparent.gif")
+div.listen(v-if="duckClicked" @click="duckInfoClicked = !duckInfoClicked")
+    img.listen(src="/img/duck-listen.gif")
+    Transition(name="link-slide")
+        a(v-if="duckInfoClicked" href="https://github.com/ChadDuck/neocities") Github
 div.space(:class="{ 'space-slide': duckClicked }")
 </template>
 
@@ -77,6 +83,14 @@ div.space(:class="{ 'space-slide': duckClicked }")
     to {
         transform: rotate(360deg);
     }
+}
+
+.link-slide-enter-from, .link-slide-leave-to {
+    transform: translateX(-100px);
+    opacity: 0
+}
+.link-slide-enter-active, .link-slide-leave-active {
+    transition: all .2s ease-in-out;
 }
 
 * {
@@ -101,6 +115,7 @@ body {
 }
 
 div.space {
+    z-index:-99;
     width:1000vw;
     height:1000vh;
     position:absolute;
@@ -114,8 +129,7 @@ div.space {
     }
 }
 
-img {
-    z-index:99;
+img.duck {
     width:50px;
     height:50px;
     cursor: pointer;
@@ -128,6 +142,36 @@ img {
 
     &.duck-rotate {
         animation: duck-rotate 10s linear infinite;
+    }
+}
+
+div.listen {
+    position:absolute;
+    bottom:0;
+    left:0;
+    margin:5px;
+
+    display:flex;
+    flex-direction:row;
+    justify-content:center;
+    align-items:center;
+    gap:5px;
+
+    width:fit-content;
+
+    cursor:help;
+
+    & > img {
+        width:100px;
+        height:auto;
+    }
+
+    & > a {
+        color:inherit;
+        text-decoration:none;
+        &:hover {
+            color:#e2d34b;
+        }
     }
 }
 </style>
